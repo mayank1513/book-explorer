97bbdf02f2a184b9d792cb3a8d1cdff9fcca4942
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-10 14:30:25 +0530

    Add lodash types and implement searchbox
    -- no styles yet

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,19 @@
-<script setup lang="ts"></script>
-<template>Hello!</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import debounce from "lodash/debounce";
+
+const apiUrl = "https://www.googleapis.com/books/v1/volumes";
+const query = ref("");
+/** Can rename if a more meaningful name is possible */
+const handleInput = debounce(async () => {
+  // todo -- may be clean query to avoid &/? etc. if it interfers with filtering etc.
+  const data = await fetch(`${apiUrl}?q=${encodeURI(query.value)}`).then(
+    (res) => res.json()
+  );
+  console.log(data);
+}, 300);
+</script>
+<template>
+  <input type="text" v-model="query" @input="handleInput" />
+</template>
 <style lang="scss"></style>

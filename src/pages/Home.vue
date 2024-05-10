0dea4e5741d42c237db005089baace11eb5f8bba
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-10 21:12:23 +0530

    refactor: Move business logic to pinia actions

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,57 +1,32 @@
 <script setup lang="ts">
 import Book from "../components/Book.vue";
 import { ref } from "vue";
-import debounce from "lodash/debounce";
-import { APIDataInterface, BookInterface } from "../types";
+import { BookInterface } from "../types";
 import BookDisplay from "../components/BookDisplay.vue";
 import InfiniteScrollLoader from "../components/InfiniteScrollLoader.vue";
-import { apiDataToBooks } from "../utils";
 import LoaderLine from "../components/LoaderLine.vue";
-import { API_BASE_URL } from "../constants";
+import { useBooksStore } from "../store";
 
-const query = ref("");
-const startIndex = ref(0);
-const totalItems = ref(0);
-const books = ref<BookInterface[]>([]);
+const bookStore = useBooksStore();
 const selectedBook = ref<BookInterface | undefined>();
-const loading = ref(true);
-/** Can rename if a more meaningful name is possible */
-const handleInput = debounce(async () => {
-  // todo -- may be clean query to avoid &/? etc. if it interfers with filtering etc.
-  loading.value = true;
-  const data: APIDataInterface = await fetch(
-    `${API_BASE_URL}?q=${encodeURI(query.value)}&startIndex=0&projection=lite`
-  ).then((res) => res.json());
-  books.value = apiDataToBooks(data);
-  totalItems.value = data.totalItems;
-  loading.value = false;
-}, 300);
-
-// Use debounce to be on safer side, e.g., when user keeps srolling top and bottom quickly
-const loadMore = debounce(async () => {
-  startIndex.value++;
-  const data: APIDataInterface = await fetch(
-    `${API_BASE_URL}?q=${encodeURI(query.value)}&startIndex=${
-      startIndex.value
-    }&projection=lite`
-  ).then((res) => res.json());
-  books.value = books.value.concat(apiDataToBooks(data));
-}, 300);
 </script>
 <template>
   <header>
-    <input type="text" v-model="query" @input="handleInput" />
-    <LoaderLine v-if="loading" />
+    <input type="text" @input="bookStore.loadBooks" />
+    <LoaderLine v-if="bookStore.loading" />
   </header>
   <main>
     <ul class="books">
       <Book
-        v-for="(book, index) in books"
+        v-for="(book, index) in bookStore.books"
         v-bind="book"
         :key="book.title + index"
         @click="selectedBook = book"
       />
-      <InfiniteScrollLoader v-if="books.length < totalItems" @load="loadMore" />
+      <InfiniteScrollLoader
+        v-if="bookStore.books.length < bookStore.totalItems"
+        @load="bookStore.loadMore"
+      />
     </ul>
     <BookDisplay
       v-if="selectedBook"
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,48 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
+import { APIDataInterface, BookInterface } from "../types";
+import debounce from "lodash/debounce";
+import { API_BASE_URL } from "../constants";
+import { apiDataToBooks } from "../utils";
 
-export const useBooksStore = defineStore("book-store", () => {});
+export const useBooksStore = defineStore("book-store", () => {
+  const query = ref("");
+  const startIndex = ref(0);
+  const totalItems = ref(0);
+  const books = ref<BookInterface[]>([]);
+  const selectedBook = ref<BookInterface | undefined>();
+  const loading = ref(true);
+
+  const loadBooks = debounce(async (e: Event) => {
+    query.value = encodeURI((e.target as HTMLInputElement).value);
+    startIndex.value = 0;
+    // todo -- may be clean query to avoid &/? etc. if it interfers with filtering etc.
+    loading.value = true;
+    const data: APIDataInterface = await fetch(
+      `${API_BASE_URL}?q=${query.value}&startIndex=0&projection=lite`
+    ).then((res) => res.json());
+    books.value = apiDataToBooks(data);
+    totalItems.value = data.totalItems;
+    loading.value = false;
+  }, 300);
+
+  // Use debounce to be on safer side, e.g., when user keeps srolling top and bottom quickly
+  const loadMore = debounce(async () => {
+    startIndex.value++;
+    const data: APIDataInterface = await fetch(
+      `${API_BASE_URL}?q=${encodeURI(query.value)}&startIndex=${
+        startIndex.value
+      }&projection=lite`
+    ).then((res) => res.json());
+    books.value = books.value.concat(apiDataToBooks(data));
+  }, 300);
+
+  return {
+    totalItems,
+    books,
+    selectedBook,
+    loading,
+    loadBooks,
+    loadMore,
+  };
+});
